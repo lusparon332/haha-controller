c60ba1b19c50932ef84a66577156fb2de18f2bc1
EternalDebug <gurin.ivan.2001@mail.ru>
2021-12-26 19:34:58 +0300

    + simple Database

--- app/models/anekdot.rb
+++ app/models/anekdot.rb
@@ -1,5 +1,5 @@
 class Anekdot < ApplicationRecord
   validates_presence_of :text
-  t.belongs_to :user
+  # belongs_to :user
   # validates_presence_of :user
 end
--- db/migrate/20211221121118_create_anekdots.rb
+++ db/migrate/20211221121118_create_anekdots.rb
@@ -2,7 +2,7 @@ class CreateAnekdots < ActiveRecord::Migration[6.1]
   def change
     create_table :anekdots do |t|
       t.text :text, null: false
-
+      # t.user :user, null: false
       t.timestamps
     end
   end
